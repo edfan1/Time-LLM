5213815439308adc1ffec046c9ba146fcbdae737
edfan1 <ethandfan@gmail.com>
2024-11-04 15:31:44 -0500

    Removed multi_gpu arg from accelerate

--- scripts/TimeLLM_snmp2018.sh
+++ scripts/TimeLLM_snmp2018.sh
@@ -11,9 +11,9 @@ d_ff=128
 
 comment='TimeLLM-snmp2018'
 
-#--main_process_port $master_port
+#--multi_gpu  --main_process_port $master_port
 
-accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process  run_main.py \
+accelerate launch --mixed_precision bf16 --num_processes $num_process run_main.py \
   --task_name long_term_forecast \
   --is_training 1 \
   --root_path ./dataset/ \
@@ -39,81 +39,81 @@ accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_proces
   --train_epochs $train_epochs \
   --model_comment $comment
 
-accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/ \
-  --data_path snmp_2018_1hourinterval.csv \
-  --model_id snmp2018_512_192 \
-  --model $model_name \
-  --data snmp2018 \
-  --features M \
-  --seq_len 512 \
-  --label_len 48 \
-  --pred_len 192 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --des 'Exp' \
-  --itr 1 \
-  --d_model 32 \
-  --d_ff 128 \
-  --batch_size $batch_size \
-  --learning_rate 0.02 \
-  --llm_layers $llama_layers \
-  --train_epochs $train_epochs \
-  --model_comment $comment
+# accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
+#   --task_name long_term_forecast \
+#   --is_training 1 \
+#   --root_path ./dataset/ \
+#   --data_path snmp_2018_1hourinterval.csv \
+#   --model_id snmp2018_512_192 \
+#   --model $model_name \
+#   --data snmp2018 \
+#   --features M \
+#   --seq_len 512 \
+#   --label_len 48 \
+#   --pred_len 192 \
+#   --factor 3 \
+#   --enc_in 7 \
+#   --dec_in 7 \
+#   --c_out 7 \
+#   --des 'Exp' \
+#   --itr 1 \
+#   --d_model 32 \
+#   --d_ff 128 \
+#   --batch_size $batch_size \
+#   --learning_rate 0.02 \
+#   --llm_layers $llama_layers \
+#   --train_epochs $train_epochs \
+#   --model_comment $comment
 
-accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/ \
-  --data_path snmp_2018_1hourinterval.csv \
-  --model_id snmp2018_512_336 \
-  --model $model_name \
-  --data snmp2018 \
-  --features M \
-  --seq_len 512 \
-  --label_len 48 \
-  --pred_len 336 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --des 'Exp' \
-  --itr 1 \
-  --d_model $d_model \
-  --d_ff $d_ff \
-  --batch_size $batch_size \
-  --lradj 'COS'\
-  --learning_rate 0.001 \
-  --llm_layers $llama_layers \
-  --train_epochs $train_epochs \
-  --model_comment $comment
+# accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
+#   --task_name long_term_forecast \
+#   --is_training 1 \
+#   --root_path ./dataset/ \
+#   --data_path snmp_2018_1hourinterval.csv \
+#   --model_id snmp2018_512_336 \
+#   --model $model_name \
+#   --data snmp2018 \
+#   --features M \
+#   --seq_len 512 \
+#   --label_len 48 \
+#   --pred_len 336 \
+#   --factor 3 \
+#   --enc_in 7 \
+#   --dec_in 7 \
+#   --c_out 7 \
+#   --des 'Exp' \
+#   --itr 1 \
+#   --d_model $d_model \
+#   --d_ff $d_ff \
+#   --batch_size $batch_size \
+#   --lradj 'COS'\
+#   --learning_rate 0.001 \
+#   --llm_layers $llama_layers \
+#   --train_epochs $train_epochs \
+#   --model_comment $comment
 
-accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/ \
-  --data_path snmp_2018_1hourinterval.csv \
-  --model_id snmp2018_512_720 \
-  --model $model_name \
-  --data snmp2018 \
-  --features M \
-  --seq_len 512 \
-  --label_len 48 \
-  --pred_len 720 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --des 'Exp' \
-  --itr 1 \
-  --d_model $d_model \
-  --d_ff $d_ff \
-  --batch_size $batch_size \
-  --learning_rate $learning_rate \
-  --llm_layers $llama_layers \
-  --train_epochs $train_epochs \
-  --model_comment $comment
+# accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
+#   --task_name long_term_forecast \
+#   --is_training 1 \
+#   --root_path ./dataset/ \
+#   --data_path snmp_2018_1hourinterval.csv \
+#   --model_id snmp2018_512_720 \
+#   --model $model_name \
+#   --data snmp2018 \
+#   --features M \
+#   --seq_len 512 \
+#   --label_len 48 \
+#   --pred_len 720 \
+#   --factor 3 \
+#   --enc_in 7 \
+#   --dec_in 7 \
+#   --c_out 7 \
+#   --des 'Exp' \
+#   --itr 1 \
+#   --d_model $d_model \
+#   --d_ff $d_ff \
+#   --batch_size $batch_size \
+#   --learning_rate $learning_rate \
+#   --llm_layers $llama_layers \
+#   --train_epochs $train_epochs \
+#   --model_comment $comment
